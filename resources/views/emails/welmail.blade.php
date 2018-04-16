5b9d2fde89e2a8b74659c3497ab600b1cda31e6c
Jorge Bastidas <jorgebastidas9@gmail.com>
2018-04-16 13:01:31 -0400

    Change

---
+++ resources/views/emails/welmail.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="{{ url('/') }}/vendor/bootstrap/css/bootstrap.min.css">
+    <link href="{{ asset('css/mainBack.css') }}" rel="stylesheet">
+  </head>
+  <body>
+    <div class="col-sm-12 col-md-8 col-md-offset-2">
+      <div class="col-sm-12">
+        <div class="panel panel-alternative">
+          <div class="panel-heading">
+            <h4>Welcome to Krypto</h4>
+          </div>
+          <div class="panel-body">
+            <p>Hello, Our system is ready for you, enter through <a href="www.krypto.finance">www.krypto.finance</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
